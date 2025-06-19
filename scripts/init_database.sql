27a68aacbc069796b4e4f27e98ddc5afef72e71e
Mouad Jaouhari <mouadpro02@gmail.com>
2025-06-19 19:00:54 +0200

    init my own version

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,42 +1,27 @@
-/*
-=============================================================
-Create Database and Schemas
-=============================================================
-Script Purpose:
-    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
-    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
-    within the database: 'bronze', 'silver', and 'gold'.
-	
-WARNING:
-    Running this script will drop the entire 'DataWarehouse' database if it exists. 
-    All data in the database will be permanently deleted. Proceed with caution 
-    and ensure you have proper backups before running this script.
-*/
-
+-- Active: 1750346197428@@localhost@1433@master
 USE master;
 GO
 
--- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
-BEGIN
-    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE DataWarehouse;
-END;
-GO
-
--- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
-GO
 
 USE DataWarehouse;
 GO
 
--- Create Schemas
-CREATE SCHEMA bronze;
+-- Créer les schémas si inexistants (T-SQL ne supporte pas "IF NOT EXISTS" pour les schémas, donc vous pouvez ignorer l'erreur si besoin ou ajouter une vérification dynamique)
+IF NOT EXISTS (
+    SELECT * FROM sys.schemas WHERE name = 'BRONZE_LAYER'
+)
+    EXEC('CREATE SCHEMA BRONZE_LAYER');
 GO
 
-CREATE SCHEMA silver;
+IF NOT EXISTS (
+    SELECT * FROM sys.schemas WHERE name = 'SILVER_LAYER'
+)
+    EXEC('CREATE SCHEMA SILVER_LAYER');
 GO
 
-CREATE SCHEMA gold;
+IF NOT EXISTS (
+    SELECT * FROM sys.schemas WHERE name = 'GOLD_LAYER'
+)
+    EXEC('CREATE SCHEMA GOLD_LAYER');
 GO
